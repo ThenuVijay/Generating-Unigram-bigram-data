41999c424f1385da57bd48fc43c1d79202da33ad
ThenuVijay <warunvijay@gmail.com>
2021-01-23 12:39:03 +0530

    Giving Input text as tamil

--- uni1tamil.py
+++ uni1tamil.py
@@ -1,12 +1,13 @@
 import regex as re
 from collections import Counter
 import csv
-file=open(r'C:\\Users\\User\\Desktop\\new1.txt',encoding="utf-8") 
+file=open(r'C:\\Users\\User\\Desktop\\Tamil Madurai.txt',encoding="utf-8") 
 content=file.read()
 #print(content)
-result = re.sub(r"[,@\'?\.$%_]", "", content)
+result = re.sub(r"'(',')',[,@\'?\.$%_]", "", content)
 x = re.findall(r"\w+", result)
 x1=re.findall(r"\b\w+\s\w+",result,overlapped=True)
+x2=re.findall(r"\b\w+\s\w+\s\w+",result,overlapped=True)
 c1=Counter(x)
 
 with open('unigram.csv', 'w', encoding='utf-8', newline='') as file:
@@ -26,4 +27,11 @@ with open('bigram.csv', 'w', encoding='utf-8', newline='') as file:
         
 file.close()        
 
+c3=Counter(x2)
+
+with open('trigram.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    for key,value in c3.items():
+        tmp=[key,value]
+        writer.writerow(tmp)
 
