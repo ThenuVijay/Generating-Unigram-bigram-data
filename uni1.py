417b35e8abbd1048849bf2afc101634247d168e0
ThenuVijay <warunvijay@gmail.com>
2021-01-24 12:37:24 +0530

    Generating unigram bigram trigram data

--- uni1.py
+++ uni1.py
@@ -2,6 +2,7 @@ import regex as re
 from collections import Counter
 file=open('C:\\Users\\User\\Desktop\\new2.txt',"r") 
 content=file.read()
+file.close()
 result = re.sub(r"[,'(',')'@\'?\.$%_]", "", content)
 x = re.findall(r"\w+", result)
 x1=re.findall(r"\b\w+\s\w+",result,overlapped=True)
@@ -9,7 +10,7 @@ x2=re.findall(r"\b\w+\s\w+\s\w+",result,overlapped=True)
 print("Unigram",Counter(x))
 print("Bigram",Counter(x1))
 print("Trigram",Counter(x2))
-file.close()
+
 
 
 
